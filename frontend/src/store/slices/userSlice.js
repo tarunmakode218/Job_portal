d2ca9ebd5b49d999c6667a397bee2f636d8704a7
Tarun Makode <tarunmakode18@gmail.com>
2025-04-11 00:38:07 +0530

    Update userSlice.js

--- frontend/src/store/slices/userSlice.js
+++ frontend/src/store/slices/userSlice.js
@@ -92,7 +92,7 @@ export const register = (data) => async (dispatch) => {
   dispatch(userSlice.actions.registerRequest());
   try {
     const response = await axios.post(
-      "https://job-portal-1-fqxr.onrender.com/api/v1/user/register",
+      "https://job-portal-3-z1ke.onrender.com/api/v1/user/register",
       data,
       {
         withCredentials: true,
@@ -110,7 +110,7 @@ export const login = (data) => async (dispatch) => {
   dispatch(userSlice.actions.loginRequest());
   try {
     const response = await axios.post(
-      "https://job-portal-1-fqxr.onrender.com/api/v1/user/login",
+      "https://job-portal-3-z1ke.onrender.com/api/v1/user/login",
       data,
       {
         withCredentials: true,
@@ -128,7 +128,7 @@ export const getUser = () => async (dispatch) => {
   dispatch(userSlice.actions.fetchUserRequest());
   try {
     const response = await axios.get(
-      "https://job-portal-1-fqxr.onrender.com/api/v1/user/getuser",
+      "https://job-portal-3-z1ke.onrender.com/api/v1/user/getuser",
       {
         withCredentials: true,
       }
@@ -142,7 +142,7 @@ export const getUser = () => async (dispatch) => {
 export const logout = () => async (dispatch) => {
   try {
     const response = await axios.get(
-      "https://job-portal-1-fqxr.onrender.com/api/v1/user/logout",
+      "https://job-portal-3-z1ke.onrender.com/api/v1/user/logout",
       {
         withCredentials: true,
       }
